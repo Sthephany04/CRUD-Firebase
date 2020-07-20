7f525e98d048d88fd9428edd510c6a25f5beddb9
Sthephany Granados <lsgranados04@gmail.com>
2020-07-19 20:33:06 -0500

    Formulario, firestore

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hola mundo')
+const db = firebase.firestore();
+
+const eventForm = document.getElementById('event-form');
+
+eventForm.addEventListener('submit', evento => {
+  evento.preventDefault();
+
+  const sport = eventForm['event-sport'].value;
+  const direction = eventForm['event-direction'].value;
+  const description = eventForm['event-description'].value;
+
+  console.log(sport, direction, description)
+})
