e3bae5fbbf9c7f0b7bc3adca16e638893f51f778
Sthephany Granados <lsgranados04@gmail.com>
2020-07-19 21:07:13 -0500

    Funcion para traer eventos de bd

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const db = firebase.firestore();
 
 const eventForm = document.getElementById('event-form');
+const eventsContainer = document.getElementById('events-container');
 
-eventForm.addEventListener('submit', evento => {
+//Funcion para guardar el evento
+const saveEvent = (sport, direction, description) => {
+  db.collection('events').doc().set({   //Peticion asincrona que me devuelve una respuesta, se crea la coleccion en Firestore
+    sport,
+    direction,
+    description
+  })
+}
+
+//Funcion para obtener los eventos
+const getEvents = () => db.collection('events').get();
+
+window.addEventListener('DOMContentLoaded', async (evento) => {
+  const querySnapshot = await getEvents(); //QuerySnapshot contiene los resultados de una consulta, segun documentacion. Un objeto que se puede recorrer.
+  querySnapshot.forEach(doc => {
+    console.log(doc.data());
+    eventsContainer.innerHTML += `<div class= "card card-body mt-2 border-primary">
+      <h3>${doc.data().sport}</h3>
+    </div>`
+  })
+})
+
+eventForm.addEventListener('submit', async evento => {
   evento.preventDefault();
 
-  const sport = eventForm['event-sport'].value;
-  const direction = eventForm['event-direction'].value;
-  const description = eventForm['event-description'].value;
+  const sport = eventForm['event-sport'];
+  const direction = eventForm['event-direction'];
+  const description = eventForm['event-description'];
 
-  console.log(sport, direction, description)
+  await saveEvent(sport.value, direction.value, description.value); 
+  
+  eventForm.reset();
+  sport.focus();  
+  
 })
